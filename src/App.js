e673c3d79cd87d948ab5993b03acaa87679c2408
Arya <aryaajitnair@gmail.com>
2022-09-27 22:19:30 +0530

    Dashboard entry setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Auth/Login";
+import Home from "./Home";
+import BaseLayout from "./BaseLayout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Login />} />
+				<Route element={<BaseLayout />}>
+					<Route exact path="/dashboard" element={<Home />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/Home/index.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Home
