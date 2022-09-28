6846c4fab7cdbd402b6d37cbc783093de5922376
Arya <aryaajitnair@gmail.com>
2022-09-28 22:13:19 +0530

    Route protection setup

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Home from "./Home";
 import BaseLayout from "./BaseLayout";
 import Register from "./Auth/Register";
 import CreateForm from "./CreateForm.js/CreateForm";
+import AuthContextProvider from "./contexts/AuthContextProvider";
+import { RequireAuth } from "./helper/RequireAuth";
+import { RequireNotAuth } from "./helper/RequireNotAuth";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route exact path="/" element={<Login />} />
-				<Route exact path="/register" element={<Register />} />
-				<Route element={<BaseLayout />}>
-					<Route exact path="/dashboard" element={<Home />} />
-					<Route exact path="/createform" element={<CreateForm />} />
-				</Route>
-			</Routes>
-		</Router>
+		<AuthContextProvider>
+			<Router>
+				<Routes>
+					<Route element={<RequireNotAuth />}>
+						<Route exact path="/login" element={<Login />} />
+						<Route exact path="/register" element={<Register />} />
+					</Route>
+					<Route element={<RequireAuth />}>
+						<Route element={<BaseLayout />}>
+							<Route exact path="/" element={<Home />} />
+							<Route exact path="/createform" element={<CreateForm />} />
+						</Route>
+					</Route>
+				</Routes>
+			</Router>
+		</AuthContextProvider>
 	);
 }
 
---
+++ src/contexts/AuthContextProvider.js
@@ -0,0 +1,46 @@
+import React, { createContext, useState, useEffect, useMemo } from "react";
+
+export const AuthContext = createContext({
+	isAuthenticated: null,
+	setIsAuthenticated: () => {},
+	user: null,
+	setUser: () => {},
+});
+
+const AuthContextProvider = ({ children }) => {
+	const [isAuthenticated, setIsAuthenticated] = useState(null);
+	const [user, setUser] = useState(null);
+
+	const loadAuthUser = () => {
+        console.log("hello")
+		const authToken = localStorage.getItem("authToken");
+		if (!authToken) {
+			setIsAuthenticated(true);
+			return;
+		}
+	};
+
+	useEffect(() => {
+        
+		if (user===null && (isAuthenticated === null || isAuthenticated === true)) {
+			loadAuthUser();
+		}
+	}, [user, isAuthenticated]);
+
+	const providerValue = useMemo(() => {
+		return {
+			isAuthenticated,
+			setIsAuthenticated,
+			user,
+			setUser,
+		};
+	}, [isAuthenticated, setIsAuthenticated, user, setUser]);
+
+	return (
+		<AuthContext.Provider value={providerValue}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export default AuthContextProvider;
